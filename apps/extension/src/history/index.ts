eefd000943c42df559e119b16ac01e7a76679ae3
BalajiLeninrajan <bleninra@uwaterloo.ca>
2026-09-19 21:19:22 -0400

    Give the timeline a line for what carat undid

--- apps/extension/src/history/index.ts
+++ apps/extension/src/history/index.ts
@@ -1,4 +1,4 @@
-export { asNavHow, describeEntry, navTarget, renderHistory, shortName, shortValue, HISTORY_LIMITS } from './entries';
+export { asNavHow, describeEntry, navTarget, renderHistory, shortName, shortValue, suggestionClause, HISTORY_LIMITS } from './entries';
 export type { HistoryEntry, NavHow } from './entries';
 export { startHistoryRecorder, HISTORY_TIMING } from './record';
 export type { HistoryRecorder, HistoryRecorderOptions } from './record';
